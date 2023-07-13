df9c804be6c8a15b5058f75737d5a7d1f6936f5b
BoringBoredom <16615327+BoringBoredom@users.noreply.github.com>
2023-07-13 19:21:55 +0300

    add event deltas

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,157 +1,192 @@
 const interaction = document.getElementById("interaction");
-const indicator = document.getElementById("indicator");
 const threshold = document.getElementById("threshold");
-const duplicateActionCounter = document.getElementById("duplicate-actions");
 
-let previousClickTimeStamp;
 let thresholdValue = threshold.value;
-let duplicateActions = 0;
 
-function checkThreshold(timeStamp) {
-  if (timeStamp - previousClickTimeStamp < thresholdValue) {
-    indicator.style.backgroundColor = "red";
-    duplicateActionCounter.textContent = ++duplicateActions;
-  } else {
-    indicator.style.backgroundColor = "green";
+const buttons = [
+  {
+    name: "Left",
+    elements: {
+      totalDown: document.querySelector(".left.down"),
+      totalUp: document.querySelector(".left.up"),
+      totalDoubleDown: document.querySelector(".left.double"),
+      minDownDownDelta: document.querySelector(".left.down-down.delta"),
+      minDownUpDelta: document.querySelector(".left.down-up.delta"),
+    },
+    totalDown: 0,
+    totalUp: 0,
+    totalDoubleDown: 0,
+    minDownDownDelta: 999999,
+    minDownUpDelta: 999999,
+    lastDownTimeStamp: 0,
+    first: true,
+  },
+  {
+    name: "Middle",
+    elements: {
+      totalDown: document.querySelector(".middle.down"),
+      totalUp: document.querySelector(".middle.up"),
+      totalDoubleDown: document.querySelector(".middle.double"),
+      minDownDownDelta: document.querySelector(".middle.down-down.delta"),
+      minDownUpDelta: document.querySelector(".middle.down-up.delta"),
+    },
+    totalDown: 0,
+    totalUp: 0,
+    totalDoubleDown: 0,
+    minDownDownDelta: 999999,
+    minDownUpDelta: 999999,
+    lastDownTimeStamp: 0,
+    first: true,
+  },
+  {
+    name: "Right",
+    elements: {
+      totalDown: document.querySelector(".right.down"),
+      totalUp: document.querySelector(".right.up"),
+      totalDoubleDown: document.querySelector(".right.double"),
+      minDownDownDelta: document.querySelector(".right.down-down.delta"),
+      minDownUpDelta: document.querySelector(".right.down-up.delta"),
+    },
+    totalDown: 0,
+    totalUp: 0,
+    totalDoubleDown: 0,
+    minDownDownDelta: 999999,
+    minDownUpDelta: 999999,
+    lastDownTimeStamp: 0,
+    first: true,
+  },
+  {
+    name: "Backward",
+    elements: {
+      totalDown: document.querySelector(".backward.down"),
+      totalUp: document.querySelector(".backward.up"),
+      totalDoubleDown: document.querySelector(".backward.double"),
+      minDownDownDelta: document.querySelector(".backward.down-down.delta"),
+      minDownUpDelta: document.querySelector(".backward.down-up.delta"),
+    },
+    totalDown: 0,
+    totalUp: 0,
+    totalDoubleDown: 0,
+    minDownDownDelta: 999999,
+    minDownUpDelta: 999999,
+    lastDownTimeStamp: 0,
+    first: true,
+  },
+  {
+    name: "Forward",
+    elements: {
+      totalDown: document.querySelector(".forward.down"),
+      totalUp: document.querySelector(".forward.up"),
+      totalDoubleDown: document.querySelector(".forward.double"),
+      minDownDownDelta: document.querySelector(".forward.down-down.delta"),
+      minDownUpDelta: document.querySelector(".forward.down-up.delta"),
+    },
+    totalDown: 0,
+    totalUp: 0,
+    totalDoubleDown: 0,
+    minDownDownDelta: 999999,
+    minDownUpDelta: 999999,
+    lastDownTimeStamp: 0,
+    first: true,
+  },
+];
+
+function handleMousedown(ev) {
+  const button = buttons[ev.button];
+
+  if (!button.first) {
+    const delta = ev.timeStamp - button.lastDownTimeStamp;
+
+    if (delta < thresholdValue) {
+      button.elements.totalDoubleDown.textContent = ++button.totalDoubleDown;
+
+      if (button.totalDoubleDown == 1) {
+        button.elements.totalDoubleDown.classList.add("warning");
+      }
+    }
+
+    if (delta < button.minDownDownDelta) {
+      button.elements.minDownDownDelta.textContent = delta.toFixed(1);
+      button.minDownDownDelta = delta;
+    }
+
+    console.log(
+      `%c${button.name}: Mousedown - Mousedown Δ: ${delta} ms`,
+      "color: black; background-color: white"
+    );
   }
 
-  previousClickTimeStamp = timeStamp;
+  button.first = false;
+
+  button.elements.totalDown.textContent = ++button.totalDown;
+  button.lastDownTimeStamp = ev.timeStamp;
 }
 
-const leftClicksDown = document.getElementById("left-clicks-down");
-let totalLeftClicksDown = 0;
+function handleMouseup(ev) {
+  const button = buttons[ev.button];
+  const delta = ev.timeStamp - button.lastDownTimeStamp;
 
-const middleClicksDown = document.getElementById("middle-clicks-down");
-let totalMiddleClicksDown = 0;
+  if (delta < button.minDownUpDelta) {
+    button.elements.minDownUpDelta.textContent = delta.toFixed(1);
+    button.minDownUpDelta = delta;
+  }
 
-const rightClicksDown = document.getElementById("right-clicks-down");
-let totalRightClicksDown = 0;
+  console.log(
+    `%c${button.name}: Mousedown - Mouseup Δ: ${delta} ms`,
+    "color: white; background-color: black"
+  );
 
-const forwardClicksDown = document.getElementById("forward-clicks-down");
-let totalForwardClicksDown = 0;
-
-const backwardClicksDown = document.getElementById("backward-clicks-down");
-let totalBackwardClicksDown = 0;
+  button.elements.totalUp.textContent = ++button.totalUp;
+}
 
 interaction.addEventListener("mousedown", (ev) => {
   ev.preventDefault();
   ev.stopPropagation();
 
-  checkThreshold(ev.timeStamp);
-
-  const button = ev.button;
-
-  switch (button) {
-    case 0:
-      leftClicksDown.textContent = ++totalLeftClicksDown;
-      break;
-    case 1:
-      middleClicksDown.textContent = ++totalMiddleClicksDown;
-      break;
-    case 2:
-      rightClicksDown.textContent = ++totalRightClicksDown;
-      break;
-    case 3:
-      backwardClicksDown.textContent = ++totalBackwardClicksDown;
-      break;
-    case 4:
-      forwardClicksDown.textContent = ++totalForwardClicksDown;
-      break;
-  }
+  handleMousedown(ev);
 });
-
-const leftClicksUp = document.getElementById("left-clicks-up");
-let totalLeftClicksUp = 0;
-
-const middleClicksUp = document.getElementById("middle-clicks-up");
-let totalMiddleClicksUp = 0;
-
-const rightClicksUp = document.getElementById("right-clicks-up");
-let totalRightClicksUp = 0;
-
-const forwardClicksUp = document.getElementById("forward-clicks-up");
-let totalForwardClicksUp = 0;
-
-const backwardClicksUp = document.getElementById("backward-clicks-up");
-let totalBackwardClicksUp = 0;
 
 interaction.addEventListener("mouseup", (ev) => {
   ev.preventDefault();
   ev.stopPropagation();
 
-  const button = ev.button;
-
-  switch (button) {
-    case 0:
-      leftClicksUp.textContent = ++totalLeftClicksUp;
-      break;
-    case 1:
-      middleClicksUp.textContent = ++totalMiddleClicksUp;
-      break;
-    case 2:
-      rightClicksUp.textContent = ++totalRightClicksUp;
-      break;
-    case 3:
-      backwardClicksUp.textContent = ++totalBackwardClicksUp;
-      break;
-    case 4:
-      forwardClicksUp.textContent = ++totalForwardClicksUp;
-      break;
-  }
+  handleMouseup(ev);
 });
 
-const scrollUps = document.getElementById("scroll-ups");
-let totalScrollUps = 0;
+const scrollUp = document.getElementById("scroll-up");
+let totalScrollUp = 0;
 
-const scrollDowns = document.getElementById("scroll-downs");
-let totalScrollDowns = 0;
+const scrollDown = document.getElementById("scroll-down");
+let totalScrollDown = 0;
 
 interaction.addEventListener("wheel", (ev) => {
   ev.preventDefault();
   ev.stopPropagation();
 
-  const delta = ev.wheelDeltaY;
-
-  if (delta > 0) {
-    scrollUps.textContent = ++totalScrollUps;
-  } else if (delta < 0) {
-    scrollDowns.textContent = ++totalScrollDowns;
+  if (ev.wheelDeltaY > 0) {
+    scrollUp.textContent = ++totalScrollUp;
+  } else if (ev.wheelDeltaY < 0) {
+    scrollDown.textContent = ++totalScrollDown;
   }
 });
 
 const pollingRate = document.getElementById("polling-rate");
-let isPolling = true;
 let moveDeltas = 0;
 let moveCounter = 0;
 let previousMoveTimeStamp = 0;
 
-function displayPollingRate(ev) {
+interaction.addEventListener("pointermove", (ev) => {
   for (const event of ev.getCoalescedEvents()) {
-    const timeStamp = event.timeStamp;
-    moveDeltas += timeStamp - previousMoveTimeStamp;
+    moveDeltas += event.timeStamp - previousMoveTimeStamp;
     moveCounter++;
-    previousMoveTimeStamp = timeStamp;
+    previousMoveTimeStamp = event.timeStamp;
 
     if (moveDeltas >= 1000) {
-      pollingRate.textContent =
-        Math.round(1000 / (moveDeltas / moveCounter)) || "?";
+      pollingRate.textContent = Math.round(1000 / (moveDeltas / moveCounter));
       moveDeltas = moveCounter = 0;
     }
   }
-}
-
-document.getElementById("stop-polling").addEventListener("click", () => {
-  if (isPolling) {
-    interaction.removeEventListener("pointermove", displayPollingRate);
-  } else {
-    interaction.addEventListener("pointermove", displayPollingRate);
-  }
-
-  isPolling = !isPolling;
-  pollingRate.textContent = "?";
 });
-
-interaction.addEventListener("pointermove", displayPollingRate);
 
 threshold.addEventListener("input", () => {
   thresholdValue = threshold.value;
